43e2e87e62510d94a98c4e69949cad1538e60e32
gongjie437 <gongjie437@gmail.com>
2024-03-28 17:27:04 -0700

    Update setDevice.cu

--- 3.1lesson/setDevice.cu
+++ 3.1lesson/setDevice.cu
@@ -6,7 +6,7 @@ int main(void)
     // 检测计算机GPU数量
     int iDeviceCount = 0;
     cudaError_t error = cudaGetDeviceCount(&iDeviceCount);
-
+//if you execute export CUDA_VISIBLE_DEVICES=0,1,2 in the same bash windows as this exeuctable, this will get GPU device count as 3 
     if (error != cudaSuccess || iDeviceCount == 0)
     {
         printf("No CUDA campatable GPU found!\n");
@@ -19,7 +19,7 @@ int main(void)
     
     // 设置执行
     int iDev = 0;
-    error = cudaSetDevice(iDev); //export cuda_visible_device, env 
+    error = cudaSetDevice(iDev); 
     if (error != cudaSuccess)
     {
         printf("fail to set GPU 0 for computing.\n");
